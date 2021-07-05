6445f58b37005e0b207a6098d599d8f866fcb98a
223yu <223yuta.jp@gmail.com>
2021-07-05 02:58:03 +0000

    Finished add the comment with ajax

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,18 +2,15 @@ class BookCommentsController < ApplicationController
 
   def create
     @book = Book.find(params[:book_id])
+    @comment = BookComment.new
     comment = current_user.book_comments.new(comment_params)
     comment.book_id = @book.id
-    if comment.save
-      redirect_to request.referrer || root_path
-    else
-      redirect_to book_path(@book)
-    end
+    comment.save
   end
 
   def destroy
     BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to request.referrer || root_path
+    @book = Book.find(params[:book_id])
   end
 
   private
